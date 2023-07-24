28d9cfd6798056e1fb221ed64d404915c7218396
xiehongchen <1754581057@qq.com>
2023-07-24 23:03:31 +0800

    小修改

--- src/views/H5/Canvas.vue
+++ src/views/H5/Canvas.vue
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <template>
   <div style="display: flex; margin: 10px 0">
     <canvas
@@ -343,3 +344,351 @@ const toggleEraserMode = () => {
   cursor: url('@/assets/vue.svg'), auto;
 }
 </style>
+=======
+<template>
+  <div style="display: flex; margin: 10px 0">
+    <canvas
+      ref="myCanvas3"
+      width="1000"
+      height="600"
+      @mousedown="startDrawing"
+      @mousemove="draw3"
+      @mouseup="stopDrawing"
+      s
+      style="border: 1px solid #000000; margin: 0 20px"
+    ></canvas>
+    <div style="display: flex; flex-direction: column; flex: 1; margin: 0 20px">
+      <div style="display: flex">
+        <div @click="clearCanvas" class="button">清除</div>
+        <div @click="saveSignature" class="button">下载</div>
+        <div @click="toggleEraserMode" class="button">
+          <span v-if="eraserMode">笔画</span>
+          <span v-else>橡皮擦</span>
+        </div>
+      </div>
+      <div class="label">笔画:</div>
+      <el-slider
+        v-model="value1"
+        @change="changeNumber"
+        :min="1"
+        :step="1"
+        show-stops
+        show-input
+      />
+      <div class="label">
+        颜色:
+        <el-color-picker v-model="color1" @change="changeColor" />
+      </div>
+    </div>
+  </div>
+  <div style="display: flex">
+    <div
+      @click="start1"
+      style="border: 1px solid; width: 70px; margin-left: 20px"
+    >
+      canvas1
+    </div>
+    <div
+      @click="start2"
+      style="border: 1px solid; width: 70px; margin-left: 20px"
+    >
+      canvas2
+    </div>
+  </div>
+
+  <div style="display: flex; margin: 20px">
+    <canvas
+      ref="myCanvas"
+      width="300"
+      height="300"
+      style="border: 1px solid #000000"
+    ></canvas>
+    <canvas
+      ref="myCanvas2"
+      width="300"
+      height="300"
+      style="border: 1px solid #000000; margin: 0 20px"
+    ></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ElMessage } from 'element-plus'
+const myCanvas: Ref<HTMLCanvasElement | null> = ref(null)
+const myCanvas2: Ref<HTMLCanvasElement | null> = ref(null)
+const myCanvas3: Ref<HTMLCanvasElement | null> = ref(null)
+const start1 = () => {
+  init()
+}
+const start2 = () => {
+  init2()
+}
+
+// canvas1
+function init() {
+  let canvas = myCanvas.value
+  if (!canvas) return
+  let ctx = canvas.getContext('2d')
+  if (!ctx) return
+  draw(ctx)
+}
+
+function draw(ctx) {
+  requestAnimationFrame(function step() {
+    drawDial(ctx) //绘制表盘
+    drawAllHands(ctx) //绘制时分秒针
+    drawTime(ctx)
+    requestAnimationFrame(step)
+  })
+}
+function drawTime(ctx) {
+  const time1 = new Date()
+
+  let s = time1.getSeconds()
+  let m = time1.getMinutes()
+  let h = time1.getHours()
+  const time2 = h + ':' + m + ':' + s
+  ctx.font = '20px sans-serif'
+  ctx.fillText(time2, 10, 20)
+}
+/*绘制时分秒针*/
+function drawAllHands(ctx) {
+  let time = new Date()
+
+  let s = time.getSeconds()
+  let m = time.getMinutes()
+  let h = time.getHours()
+
+  let pi = Math.PI
+  let secondAngle = (pi / 180) * 6 * s //计算出来s针的弧度
+  let minuteAngle = (pi / 180) * 6 * m + secondAngle / 60 //计算出来分针的弧度
+  let hourAngle = (pi / 180) * 30 * h + minuteAngle / 12 //计算出来时针的弧度
+
+  drawHand(hourAngle, 60, 6, 'red', ctx) //绘制时针
+  drawHand(minuteAngle, 106, 4, 'green', ctx) //绘制分针
+  drawHand(secondAngle, 129, 2, 'blue', ctx) //绘制秒针
+}
+/*绘制时针、或分针、或秒针
+ * 参数1：要绘制的针的角度
+ * 参数2：要绘制的针的长度
+ * 参数3：要绘制的针的宽度
+ * 参数4：要绘制的针的颜色
+ * 参数4：ctx
+ * */
+function drawHand(angle, len, width, color, ctx) {
+  ctx.save()
+  ctx.translate(150, 150) //把坐标轴的远点平移到原来的中心
+  ctx.rotate(-Math.PI / 2 + angle) //旋转坐标轴。 x轴就是针的角度
+  ctx.beginPath()
+  ctx.moveTo(-4, 0)
+  ctx.lineTo(len, 0) // 沿着x轴绘制针
+  ctx.lineWidth = width
+  ctx.strokeStyle = color
+  ctx.lineCap = 'round'
+  ctx.stroke()
+  ctx.closePath()
+  ctx.restore()
+}
+
+/*绘制表盘*/
+function drawDial(ctx) {
+  let pi = Math.PI
+
+  ctx.clearRect(0, 0, 300, 300) //清除所有内容
+  ctx.save()
+
+  ctx.translate(150, 150) //一定坐标原点到原来的中心
+  ctx.beginPath()
+  ctx.arc(0, 0, 148, 0, 2 * pi) //绘制圆周
+  ctx.stroke()
+  ctx.closePath()
+
+  for (let i = 0; i < 60; i++) {
+    //绘制刻度。
+    ctx.save()
+    ctx.rotate(-pi / 2 + (i * pi) / 30) //旋转坐标轴。坐标轴x的正方形从 向上开始算起
+    ctx.beginPath()
+    ctx.moveTo(110, 0)
+    ctx.lineTo(140, 0)
+    ctx.lineWidth = i % 5 ? 2 : 4
+    ctx.strokeStyle = i % 5 ? 'blue' : 'red'
+    ctx.stroke()
+    ctx.closePath()
+    ctx.restore()
+  }
+  ctx.restore()
+}
+
+// canvas2
+let sun: any = null
+let earth: any = null
+let moon: any = null
+let ctx: any = null
+function init2() {
+  sun = new Image()
+  earth = new Image()
+  moon = new Image()
+  sun.src = 'src/assets/vue.svg'
+  earth.src = 'src/assets/vue.svg'
+  moon.src = 'src/assets/vue.svg'
+
+  let canvas = myCanvas2.value
+  if (!canvas) return
+  ctx = canvas.getContext('2d')
+  if (!ctx) return
+
+  sun.onload = function () {
+    draw2()
+  }
+}
+function draw2() {
+  ctx.clearRect(0, 0, 300, 300) //清空所有的内容
+  /*绘制 太阳*/
+  ctx.drawImage(sun, 0, 0, 300, 300)
+
+  ctx.save()
+  ctx.translate(150, 150)
+
+  // //绘制earth轨道
+  ctx.beginPath()
+  ctx.strokeStyle = 'rgba(255,255,0,0.5)'
+  ctx.arc(0, 0, 100, 0, 2 * Math.PI)
+  ctx.stroke()
+
+  let time = new Date()
+  // //绘制地球
+  ctx.rotate(
+    ((2 * Math.PI) / 60) * time.getSeconds() +
+      ((2 * Math.PI) / 60000) * time.getMilliseconds(),
+  )
+  ctx.translate(100, 0)
+  ctx.drawImage(earth, -12, -12)
+
+  // //绘制月球轨道
+  ctx.beginPath()
+  ctx.strokeStyle = 'rgba(255,255,255,.3)'
+  ctx.arc(0, 0, 40, 0, 2 * Math.PI)
+  ctx.stroke()
+
+  // //绘制月球
+  ctx.rotate(
+    ((2 * Math.PI) / 6) * time.getSeconds() +
+      ((2 * Math.PI) / 6000) * time.getMilliseconds(),
+  )
+  ctx.translate(40, 0)
+  ctx.drawImage(moon, -3.5, -3.5)
+  ctx.restore()
+
+  requestAnimationFrame(draw2)
+}
+
+// canvas3
+const eraserMode = ref(false)
+const drawing = ref(false)
+let lastX = 0
+let lastY = 0
+let context: any
+const value1 = ref(1)
+const color1 = ref('#000')
+
+function startDrawing(e: any) {
+  drawing.value = true
+  let canvas = myCanvas3.value
+  if (!canvas) return
+  context = canvas.getContext('2d')
+  if (!context) return
+  lastX = e.clientX - context.canvas.offsetLeft
+  lastY = e.clientY - context.canvas.offsetTop
+}
+function draw3(e: any) {
+  if (!drawing.value) return
+  if (!context) return
+  const newX = e.clientX - context.canvas.offsetLeft
+  const newY = e.clientY - context.canvas.offsetTop
+
+  context.beginPath()
+  context.moveTo(lastX, lastY)
+  context.lineTo(newX, newY)
+  context.closePath()
+  context.stroke()
+
+  lastX = newX
+  lastY = newY
+}
+function stopDrawing() {
+  drawing.value = false
+}
+function clearCanvas() {
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+  ElMessage({
+    message: '清除成功',
+    type: 'success',
+  })
+}
+function saveSignature() {
+  const dataUrl = context.canvas.toDataURL()
+  // 在这里可以将Data URL发送到服务器保存或执行其他操作
+  // window.open(dataUrl)
+  const a = document.createElement('a')
+  a.download = '11'
+  a.href = dataUrl
+  a.click()
+  ElMessage({
+    message: '下载成功',
+    type: 'success',
+  })
+}
+
+const changeNumber = () => {
+  console.log(value1)
+  context.lineWidth = value1.value
+}
+
+const changeColor = () => {
+  context.strokeStyle = color1.value
+}
+
+const toggleEraserMode = () => {
+  eraserMode.value = !eraserMode.value
+  if (eraserMode.value) {
+    context.strokeStyle = 'white'
+    context.lineWidth = 100
+    context.canvas.classList.add('canvas-eraser')
+    ElMessage({
+      message: '橡皮擦模式',
+      type: 'success',
+    })
+  } else {
+    context.canvas.classList.remove('canvas-eraser')
+    context.lineWidth = value1.value
+    context.strokeStyle = color1.value
+    ElMessage({
+      message: '笔画模式',
+      type: 'success',
+    })
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.button {
+  border: 2px solid red;
+  height: 30px;
+  width: auto;
+  margin: 0 10px;
+  text-align: center;
+  line-height: 30px;
+}
+
+.label {
+  color: red;
+  margin: 10px;
+  font-size: 20px;
+  font-weight: 700;
+}
+
+.canvas-eraser {
+  cursor: url('@/assets/vue.svg'), auto;
+}
+</style>
+>>>>>>> d630093 (小修改)
--- src/views/new/index.vue
+++ src/views/new/index.vue
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <template>
   <div id="test">
     <div class="label">
@@ -123,3 +124,579 @@ button {
   margin: 0 20px;
 }
 </style>
+=======
+<template>
+  <div class="blog-box">
+    <div class="blog-content">
+      <div class="title">
+        <div>全部博客</div>
+        <div>
+          共
+          <span style="color: aquamarine">{{ number }}</span>
+          篇
+        </div>
+      </div>
+      <div class="card-box">
+        <div
+          class="card"
+          v-for="item in blogList"
+          @click="goDetail(item.id)"
+          :key="item.id"
+        >
+          <div class="card-img">
+            <img src="@/assets/vue.svg" />
+          </div>
+          <div class="card-right">
+            <div class="card-title">
+              {{ item.title }}
+            </div>
+            <div class="card-introduction">
+              {{ item.introduction }}
+            </div>
+            <div class="card-bottom">
+              <div class="user-img">
+                <img src="@/assets/vue.svg" />
+              </div>
+              <div class="user">
+                {{ item.author }}
+              </div>
+              <div class="time">
+                {{ item.time }}
+              </div>
+              <div class="tags">
+                <el-tag
+                  class="tag"
+                  v-for="i in item.tag"
+                  @click.stop="clickTag(i)"
+                  :key="i"
+                >
+                  {{ i }}
+                </el-tag>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- <el-pagination
+        class="pagination"
+        background
+        layout="prev, pager, next"
+        :total="blogList.length"
+      /> -->
+    </div>
+    <div class="blog-right">
+      <div class="blog-classification">
+        <div class="class-title">分类</div>
+        <div class="class-box">
+          <div
+            class="class-item"
+            v-for="item in classList"
+            @click="clickClass(item.title)"
+            :key="item.id"
+          >
+            <div class="class-img">
+              <img src="@/assets/vue.svg" />
+            </div>
+            <div class="class-name">
+              {{ item.title }}
+            </div>
+            <div class="class-num">
+              {{ item.number }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="blog-label">
+        <div class="label-title">标签</div>
+        <div class="container">
+          <div class="tag-box">
+            <span
+              class="tag-item"
+              v-for="item in styleTag"
+              :key="item.id"
+              :style="{
+                fontWeight: item.weight,
+                fontSize: item.size + 'px',
+                color: item.color,
+                height: item.size + 'px',
+              }"
+              @click="clickTag(item.title)"
+            >
+              {{ item.title }}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const number = ref(111)
+const blogList = reactive([
+  {
+    id: 1,
+    title: '123',
+    introduction:
+      '只要卡住这个17毫秒，每隔17毫秒进行操作，就能确保动画丝滑dsad大大大大adddd啊实打实大师大师d啊实打实大师dddddddddsad撒啊实打实大师大多asdasss',
+    author: 'xie',
+    imgUrl: '@/assets/vue.svg',
+    time: '2000-10-1',
+    tag: ['abc', 'bda', 'dada'],
+  },
+  {
+    id: 2,
+    title: '123',
+    introduction: '只要卡住这个17毫秒，每隔17毫秒进行操作，就能确保动画丝滑',
+    author: 'xie',
+    imgUrl: '@/assets/vue.svg',
+    time: '2000-10-1',
+    tag: ['abc', 'bda', 'dada'],
+  },
+  {
+    id: 3,
+    title: '123',
+    introduction: '只要卡住这个17毫秒，每隔17毫秒进行操作，就能确保动画丝滑',
+    author: 'xie',
+    imgUrl: '@/assets/vue.svg',
+    time: '2000-10-1',
+    tag: ['abc', 'bda', 'dada'],
+  },
+  {
+    id: 4,
+    title: '123',
+    introduction: '只要卡住这个17毫秒，每隔17毫秒进行操作，就能确保动画丝滑',
+    author: 'xie',
+    imgUrl: '@/assets/vue.svg',
+    time: '2000-10-1',
+    tag: ['abc', 'bda', 'dada'],
+  },
+  {
+    id: 5,
+    title: '123',
+    introduction: '只要卡住这个17毫秒，每隔17毫秒进行操作，就能确保动画丝滑',
+    author: 'xie',
+    imgUrl: '@/assets/vue.svg',
+    time: '2000-10-1',
+    tag: ['abc', 'bda', 'dada'],
+  },
+  {
+    id: 6,
+    title: '123',
+    introduction: '只要卡住这个17毫秒，每隔17毫秒进行操作，就能确保动画丝滑',
+    author: 'xie',
+    imgUrl: '@/assets/vue.svg',
+    time: '2000-10-1',
+    tag: ['abc', 'bda', 'dada'],
+  },
+  {
+    id: 7,
+    title: '123',
+    introduction: '只要卡住这个17毫秒，每隔17毫秒进行操作，就能确保动画丝滑',
+    author: 'xie',
+    imgUrl: '@/assets/vue.svg',
+    time: '2000-10-1',
+    tag: ['abc', 'bda', 'dada'],
+  },
+  {
+    id: 8,
+    title: '123',
+    introduction: '只要卡住这个17毫秒，每隔17毫秒进行操作，就能确保动画丝滑',
+    author: 'xie',
+    imgUrl: '@/assets/vue.svg',
+    time: '2000-10-1',
+    tag: ['abc', 'bda', 'dada'],
+  },
+  {
+    id: 9,
+    title: '123',
+    introduction: '只要卡住这个17毫秒，每隔17毫秒进行操作，就能确保动画丝滑',
+    author: 'xie',
+    imgUrl: '@/assets/vue.svg',
+    time: '2000-10-1',
+    tag: ['abc', 'bda', 'dada'],
+  },
+  {
+    id: 10,
+    title: '123',
+    introduction: '只要卡住这个17毫秒，每隔17毫秒进行操作，就能确保动画丝滑',
+    author: 'xie',
+    imgUrl: '@/assets/vue.svg',
+    time: '2000-10-1',
+    tag: ['abc', 'bda', 'dada'],
+  },
+  {
+    id: 11,
+    title: '11',
+    introduction: '只要卡住这个17毫秒，每隔17毫秒进行操作，就能确保动画丝滑',
+    author: 'xie',
+    imgUrl: '@/assets/vue.svg',
+    time: '2000-10-1',
+    tag: ['abc', 'bda', 'dada'],
+  },
+])
+const classList = reactive([
+  {
+    id: 1,
+    imgUrl: '',
+    title: 'Java',
+    number: 11,
+  },
+  {
+    id: 2,
+    imgUrl: '',
+    title: 'JavaScript',
+    number: 11,
+  },
+  {
+    id: 3,
+    imgUrl: '',
+    title: 'Python',
+    number: 11,
+  },
+  {
+    id: 4,
+    imgUrl: '',
+    title: 'c++',
+    number: 11,
+  },
+  {
+    id: 5,
+    imgUrl: '',
+    title: 'c++',
+    number: 11,
+  },
+  {
+    id: 6,
+    imgUrl: '',
+    title: 'c++',
+    number: 11,
+  },
+  {
+    id: 7,
+    imgUrl: '',
+    title: 'c++',
+    number: 11,
+  },
+])
+const clickTag = (item: string) => {
+  selectblog('', item)
+}
+const clickClass = (item: string) => {
+  selectblog(item, '')
+}
+function selectblog(classType?: string, label?: string) {
+  console.log(classType)
+  console.log(label)
+}
+const router = useRouter()
+const goDetail = (id: number) => {
+  router.push(`/detail?id=${id}`)
+}
+const tagList = reactive([
+  {
+    id: 0,
+    title: 'css',
+    number: 12,
+  },
+  {
+    id: 1,
+    title: 'css',
+    number: 12,
+  },
+  {
+    id: 2,
+    title: 'js',
+    number: 16,
+  },
+  {
+    id: 3,
+    title: 'ts',
+    number: 4,
+  },
+  {
+    id: 4,
+    title: 'java',
+    number: 5,
+  },
+  {
+    id: 5,
+    title: 'c',
+    number: 15,
+  },
+  {
+    id: 6,
+    title: 'py',
+    number: 25,
+  },
+  {
+    id: 7,
+    title: 'c++',
+    number: 5,
+  },
+  {
+    id: 4,
+    title: 'java',
+    number: 5,
+  },
+  {
+    id: 4,
+    title: 'java',
+    number: 5,
+  },
+  {
+    id: 4,
+    title: 'java',
+    number: 5,
+  },
+  {
+    id: 4,
+    title: 'java',
+    number: 5,
+  },
+  {
+    id: 4,
+    title: 'java',
+    number: 5,
+  },
+])
+const styleTag = ref([
+  {
+    id: 0,
+    title: '',
+    number: 0,
+    size: 0,
+    weight: 0,
+    color: '',
+  },
+])
+let weight = 0
+let size = 0
+onMounted(() => {
+  tagList.forEach((item) => {
+    console.log(item)
+    if (item.number < 5) {
+      size = 10
+      weight = 300
+    } else if (item.number < 10) {
+      size = 15
+      weight = 400
+    } else {
+      size = 20
+      weight = 600
+    }
+    styleTag.value.push({
+      id: item.id,
+      title: item.title,
+      number: item.number,
+      size: size,
+      weight: weight,
+      color:
+        'rgb(' +
+        parseInt(Math.random() * 255 + '') +
+        ',' +
+        parseInt(Math.random() * 255 + '') +
+        ',' +
+        parseInt(Math.random() * 255 + '') +
+        ')',
+    })
+  })
+  styleTag.value.shift()
+})
+</script>
+
+<style lang="scss" scoped>
+::-webkit-scrollbar {
+  /*隐藏滚轮*/
+  display: none;
+}
+
+.blog-box {
+  background-color: pink;
+  height: calc(100vh - 60px);
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+
+  .blog-content {
+    width: 50%;
+    height: 100%;
+    padding: 20px;
+    border: 30px;
+    background-color: #fff;
+    border-radius: 10px;
+    overflow: scroll;
+
+    .title {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 20px;
+      font-size: 20px;
+    }
+
+    .card-box {
+      padding: 5px 5px;
+
+      .card {
+        width: 100%;
+        height: 150px;
+        border-bottom: 2px solid bisque;
+        display: flex;
+
+        .card-img {
+          padding: 5px;
+          width: 150px;
+          height: 100%;
+
+          img {
+            width: 100%;
+          }
+        }
+
+        .card-right {
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+
+          .card-title {
+            margin: 5px;
+            font-size: 20px;
+            font-weight: 700;
+          }
+
+          .card-introduction {
+            margin: 5px;
+            font-size: 15px;
+            font-weight: 500;
+            flex: 1;
+            line-height: 1.5;
+            text-indent: 2em;
+          }
+
+          .card-bottom {
+            height: 20px;
+            margin: 5px;
+            display: flex;
+
+            .user-img {
+              width: 20px;
+              height: 100%;
+              border-radius: 50%;
+              overflow: hidden;
+              margin-right: 10px;
+
+              img {
+                width: 100%;
+              }
+            }
+
+            .user {
+              color: red;
+              margin-right: 20px;
+            }
+
+            .time {
+              line-height: 20px;
+            }
+
+            .tags {
+              flex: 1;
+              display: flex;
+              justify-content: flex-end;
+
+              .tag {
+                margin: 0 5px;
+                cursor: pointer;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    .pagination {
+      padding: 10px 0;
+      display: flex;
+      width: 100%;
+      justify-content: center;
+    }
+  }
+
+  .blog-right {
+    flex-direction: column;
+    width: 20%;
+    padding: 0 20px;
+
+    .blog-classification {
+      background-color: #fff;
+      border-radius: 10px;
+      height: 300px;
+      margin-bottom: 20px;
+      padding: 5px;
+
+      .class-title {
+        text-align: center;
+        font-size: 20px;
+        font-weight: 600;
+        margin-bottom: 10px;
+      }
+
+      .class-box {
+        height: 250px;
+        overflow: scroll;
+
+        .class-item {
+          display: flex;
+          height: 50px;
+          justify-content: space-between;
+          align-items: center;
+          padding: 5px;
+          cursor: pointer;
+
+          .class-img {
+            height: 100%;
+            width: 40px;
+            border-radius: 50%;
+            overflow: hidden;
+
+            img {
+              width: 100%;
+            }
+          }
+        }
+      }
+    }
+
+    .blog-label {
+      background-color: #fff;
+      border-radius: 10px;
+      height: 300px;
+      margin-bottom: 20px;
+      padding: 5px;
+
+      .label-title {
+        text-align: center;
+        font-size: 20px;
+        font-weight: 600;
+        margin-bottom: 10px;
+      }
+
+      .container {
+        height: calc(100% - 30px);
+
+        .tag-box {
+          width: 100%;
+          height: 100%;
+
+          .tag-item {
+            margin: 5px;
+            cursor: pointer;
+            word-wrap: break-word;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+>>>>>>> d630093 (小修改)
