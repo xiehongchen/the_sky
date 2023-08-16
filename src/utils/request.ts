e23e2caa635c6d987ab4053c9e8736ed4e1607b2
liaozhaozhou <liaozhaozhou@ecyao.com>
2023-08-16 20:38:48 +0800

    request使用ts封装

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,40 +1,57 @@
 //进行axios二次封装:使用请求与响应拦截器
 import axios from 'axios'
+import type {
+  AxiosInstance,
+  AxiosError,
+  InternalAxiosRequestConfig,
+  AxiosResponse,
+} from 'axios'
 import { ElMessage } from 'element-plus'
 //引入用户相关的仓库
 // import useUserStore from '@/store/userStore'
 //第一步:利用axios对象的create方法,去创建axios实例(其他的配置:基础路径、超时的时间)
-const request = axios.create({
+const request: AxiosInstance = axios.create({
   //基础路径
   baseURL: import.meta.env.VITE_APP_BASE_API, //基础路径上会携带/api
   timeout: 5000, //超时的时间的设置
 })
 //第二步:request实例添加请求与响应拦截器
-request.interceptors.request.use((config) => {
-  //获取用户相关的小仓库:获取仓库内部token,登录成功以后携带给服务器
-  // const userStore = useUserStore()
-  // if (userStore.token) {
-  //   config.headers.token = userStore.token
-  // }
-  //config配置对象,headers属性请求头,经常给服务器端携带公共参数
-  //返回配置对象
-  return config
-})
+request.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    //获取用户相关的小仓库:获取仓库内部token,登录成功以后携带给服务器
+    // const userStore = useUserStore()
+    // if (userStore.token) {
+    //   config.headers.token = userStore.token
+    // }
+    //config配置对象,headers属性请求头,经常给服务器端携带公共参数
+    //返回配置对象
+    return config
+  },
+  (error: AxiosError) => {
+    console.log(error)
+    return Promise.reject(error)
+  },
+)
 
 //第三步:响应拦截器
 request.interceptors.response.use(
-  (response) => {
+  (response: AxiosResponse) => {
     //成功回调
     //简化数据
-    return response.data
+    const { code, msg, data } = response.data
+    if (code === 0) {
+      return data
+    } else {
+      return Promise.reject(new Error(msg))
+    }
   },
-  (error) => {
+  (error: AxiosError) => {
     console.log(error)
     //失败回调:处理http网络错误的
     //定义一个变量:存储网络错误信息
     let message = ''
     //http状态码
-    const status = error.response.status
+    const status = error.response?.status
     switch (status) {
       case 401:
         message = 'TOKEN过期'
@@ -62,3 +79,17 @@ request.interceptors.response.use(
 )
 //对外暴露
 export default request
+
+export const http = {
+  async get<T = any>(
+    url: string,
+    data?: object,
+    config?: InternalAxiosRequestConfig,
+  ): Promise<T> {
+    const response: AxiosResponse<T> = await request.get(url, {
+      params: data,
+      ...config,
+    })
+    return response.data
+  },
+}
