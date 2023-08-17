b7c7c015e11075f8d9f31ecf56cb58c0212c325c
liaozhaozhou <liaozhaozhou@ecyao.com>
2023-08-17 18:53:17 +0800

    仓库

---
+++ src/views/repository/components/ListTools.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="box">
+    <div class="item" v-for="item in list" :key="item.id">
+      <img class="img" :src="item.imgUrl" />
+      <el-link :href="item.url" target="_blank">{{ item.title }}</el-link>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface list11 {
+  id: number
+  title: string
+  url: string
+  imgUrl: string
+}
+const props = defineProps({ list: Array as PropType<list11[]> })
+console.log(props)
+</script>
+
+<style lang="scss" scoped>
+.box {
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  .item {
+    margin: 5px 0;
+    width: 300px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    box-sizing: border-box;
+    .img {
+      height: 40px;
+    }
+    :deep(.el-link__inner) {
+      font-size: 18px;
+      font-weight: bold;
+      margin-left: 10px;
+    }
+  }
+}
+</style>
--- src/views/repository/index.vue
+++ src/views/repository/index.vue
@@ -1,126 +1,37 @@
 <template>
-  <div class="repository-box">
-    <el-form :model="searchForm" inline class="form-box">
-      <el-form-item label="标题" style="margin-left: 10px">
-        <el-input v-model="searchForm.title" placeholder="请输入标题" style="width: 200px" />
-      </el-form-item>
-      <el-form-item label="状态">
-        <el-select v-model="searchForm.state" placeholder="请选择状态">
-          <el-option label="成功" value="成功" />
-          <el-option label="失败" value="失败" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="标签">
-        <el-select v-model="searchForm.label" placeholder="请选择标签">
-          <el-option label="成功" value="成功" />
-          <el-option label="失败" value="失败" />
-        </el-select>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">搜索</el-button>
-      </el-form-item>
-    </el-form>
-    <el-tabs v-model="activeName" type="border-card" @tab-click="handleClick">
-      <el-tab-pane v-for="item in tabsList" :label="item.title" :name="item.title" :key="item.id">
-        <el-table :data="tableData" stripe border>
-          <el-table-column prop="title" label="标题" width="180"></el-table-column>
-          <el-table-column prop="author" label="作者" width="120"></el-table-column>
-          <el-table-column prop="createTime" label="创建时间" width="180"></el-table-column>
-          <el-table-column prop="introduction" label="简介" width="180"></el-table-column>
-          <el-table-column prop="address" label="状态" width="180">
-            <template #default="scope">
-              <el-tag :type="scope.row.stateType === 'Home' ? '' : 'success'" disable-transitions>
-                {{ scope.row.state }}
-              </el-tag>
-            </template>
-          </el-table-column>
-          <el-table-column prop="address" label="标签">
-            <template #default="scope">
-              <el-tag :type="scope.row.tagType === 'Home' ? '' : 'success'" disable-transitions>
-                {{ scope.row.tag }}
-              </el-tag>
-            </template>
-          </el-table-column>
-        </el-table>
-      </el-tab-pane>
-      <el-pagination v-model:current-page="currentPage2" v-model:page-size="pageSize2" :page-sizes="[10, 20, 30, 50]"
-        :small="small" :disabled="disabled" :background="background" layout="prev, pager, next, jumper,->, sizes, total"
-        :total="1000" @size-change="handleSizeChange" @current-change="handleCurrentChange" style="margin: 10px 0;"/>
-    </el-tabs>
+  <div class="collapse">
+    <el-collapse v-model="activeNames" @change="handleChange">
+      <el-collapse-item title="常用" name="1">
+        <ListTools :list="tools"></ListTools>
+      </el-collapse-item>
+      <el-collapse-item title="文档" name="2">
+        <ListTools :list="document"></ListTools>
+      </el-collapse-item>
+      <el-collapse-item title="Efficiency" name="3">
+        <ListTools :list="tools"></ListTools>
+      </el-collapse-item>
+      <el-collapse-item title="Controllability" name="4">
+        <ListTools :list="tools"></ListTools>
+      </el-collapse-item>
+    </el-collapse>
   </div>
 </template>
 
-<script setup lang="ts">
-import { tabsList, tableDataList } from './config/common.ts'
-
-onMounted(() => {
-  getData(searchParams.value)
-})
-
-// 搜索
-const searchForm = ref<any>({
-  title: '',
-  state: '',
-  label: '',
-})
-const onSubmit = () => {
-  console.log(searchForm.value)
-}
-// tabs
-const activeName = ref<string>('JS')
-const searchParams = ref<any>({
-  tab: 'JS',
-  page: 1,
-  size: 10,
-})
-const handleClick = (tab: any) => {
-  console.log(tab.paneName)
-  searchParams.value.tab = tab.paneName
-  getData(searchParams.value)
-}
-// 表格
-const tableData = ref<any[]>([])
-
-function getData(params: any) {
-  console.log(params)
-  console.log(tableDataList)
-  if (params.tab === 'JS') {
-    tableData.value = tableDataList.slice(0, 1)
-  } else if (params.tab === 'CSS') {
-    tableData.value = tableDataList.slice(1, 2)
-  } else if (params.tab === 'HTML') {
-    tableData.value = tableDataList.slice(2, 5)
-  } else if (params.tab === 'Vue') {
-    tableData.value = tableDataList.slice(5, 7)
-  } else if (params.tab === 'React') {
-    tableData.value = tableDataList.slice(7, 8)
-  } else if (params.tab === 'Nodejs') {
-    tableData.value = tableDataList.slice(8, 10)
-  }
-  console.log(tableData.value)
-}
-const currentPage2 = ref(5)
-const pageSize2 = ref(10)
-const small = ref(false)
-const background = ref(true)
-const disabled = ref(false)
-const handleSizeChange = (val: number) => {
-  console.log(`${val} items per page`)
-}
-const handleCurrentChange = (val: number) => {
-  console.log(`current page: ${val}`)
+<script lang="ts" setup>
+import { tools, document } from './config.ts'
+import ListTools from './components/ListTools.vue'
+const activeNames = ref(['1'])
+const handleChange = (val: string[]) => {
+  console.log(val)
 }
 </script>
-
-<style scoped lang="scss">
-.repository-box {
-  height: 100%;
-  width: 100%;
-  border-top: 0;
-
-  .form-box {
-    margin-top: 10px;
-    border-top: 0;
+<style lang="scss" scoped>
+.collapse {
+  padding: 20px;
+  :deep(.el-collapse) {
+    .el-collapse-item__header {
+      font-size: 20px;
+    }
   }
 }
 </style>
