a5136bad821b6a1220b5a76b7a60f37fb5de4276
liaozhaozhou <liaozhaozhou@ecyao.com>
2023-08-23 14:47:42 +0800

    env文件换位置，修改文章仓库内容

--- src/views/repository/warehouseDetail/config.ts
+++ src/views/repository/warehouseDetail/config.ts
@@ -2,30 +2,37 @@ export const tabsList = [
   {
     id: 1,
     title: 'JS',
+    value: 'js',
   },
   {
     id: 2,
     title: 'CSS',
+    value: 'css',
   },
   {
     id: 3,
     title: 'Vue',
+    value: 'vue',
   },
   {
     id: 4,
     title: 'Nodejs',
+    value: 'nodejs',
   },
   {
     id: 5,
     title: 'React',
+    value: 'react',
   },
   {
     id: 6,
     title: '计算机网络',
+    value: 'network',
   },
   {
     id: 7,
     title: '算法',
+    value: 'algorithm',
   },
 ]
 export const tableDataList = [
@@ -34,7 +41,9 @@ export const tableDataList = [
     title: '111',
     author: '12',
     createTime: '111',
-    introduction: '222',
-    website: '11',
+    introduction: '关于test的大大大撒大家啊较大金万达',
+    website: 'https://juejin.cn/frontend',
+    status: 1,
+    note: 'https://juejin.cn/frontend',
   },
 ]
--- src/views/repository/warehouseDetail/index.vue
+++ src/views/repository/warehouseDetail/index.vue
@@ -30,11 +30,7 @@
         :key="item.id"
       >
         <el-table :data="tableDataList" stripe border>
-          <el-table-column
-            prop="title"
-            label="标题"
-            width="180"
-          ></el-table-column>
+          <el-table-column prop="title" label="标题"></el-table-column>
           <el-table-column
             prop="author"
             label="作者"
@@ -45,17 +41,46 @@
             label="创建时间"
             width="180"
           ></el-table-column>
-          <el-table-column
-            prop="website"
-            label="网站"
-            width="180"
-          ></el-table-column>
-          <el-table-column prop="introduction" label="简介">
-            <template v-if="isEdit">
-              <el-input v-model="introduction" placeholder="请输入" />
+          <el-table-column prop="website" label="源址">
+            <template #default="scope">
+              <el-link :href="scope.row.website" target="_blank">
+                {{ scope.row.website }}
+              </el-link>
             </template>
           </el-table-column>
-          <el-table-column label="操作" fixed="right">
+          <el-table-column prop="introduction" label="简介" width="260">
+            <template #default="scope">
+              <el-input
+                v-model="scope.row.introduction"
+                v-if="isEdit"
+              ></el-input>
+              <span v-else>{{ scope.row.introduction }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="note" label="笔记">
+            <template #default="scope">
+              <el-input v-model="scope.row.note" v-if="isEdit"></el-input>
+              <el-link :href="scope.row.note" target="_blank" v-else>
+                {{ scope.row.note }}
+              </el-link>
+            </template>
+          </el-table-column>
+          <el-table-column prop="status" label="状态">
+            <template #default="scope">
+              <el-tag class="ml-2" type="success" v-if="scope.row.status === 1">
+                总结
+              </el-tag>
+              <el-tag
+                class="ml-2"
+                type="warning"
+                v-if="scope.row.status === 0"
+                @click="changeStatus(scope.row.status)"
+              >
+                保存
+              </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
             <div class="operate">
               <span class="item" @click="editArticle">编辑</span>
               <span class="item" @click="complete" v-if="isEdit">完成</span>
@@ -83,11 +108,11 @@
 
 <script setup lang="ts">
 import { tabsList, tableDataList } from './config.ts'
-import api from '@/api'
+// import api from '@/api'
 
-onMounted(() => {
-  getData(searchParams.value)
-})
+// onMounted(() => {
+//   getData(searchParams.value)
+// })
 
 // 搜索
 const searchForm = ref<any>({
@@ -97,22 +122,25 @@ const onSubmit = () => {
   console.log(searchForm.value)
 }
 // tabs
-const activeName = ref<string>('Vue')
+const activeName = ref<string>('JS')
 const searchParams = ref<any>({
-  tab: 'JS',
+  tab: 'js',
   page: 1,
   size: 10,
 })
 const handleClick = (tab: any) => {
-  console.log(tab.paneName)
-  searchParams.value.tab = tab.paneName
+  console.log(tab)
+  searchParams.value.tab = tabsList.find(
+    (item) => item.title === tab.paneName,
+  )?.value
   getData(searchParams.value)
 }
 // 表格
 // const tableData = ref<any[]>([])
 
 function getData(params: any) {
-  api.repository.getAllArticle(params)
+  console.log(params)
+  // api.repository.getAllArticle(params)
 }
 const pagination = reactive({
   currentPage: 5,
@@ -138,6 +166,9 @@ const complete = () => {
   console.log(introduction.value)
   isEdit.value = false
 }
+const changeStatus = (status: number) => {
+  console.log(status)
+}
 </script>
 
 <style scoped lang="scss">
@@ -159,6 +190,10 @@ const complete = () => {
       color: #2dd6d7;
       cursor: pointer;
     }
+  }
+  .tags {
+    font-size: 18px;
+    margin-right: 5px;
   }
 }
 </style>
