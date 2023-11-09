c34badc7deabe2f4aa140f1c6e82bf75b348b877
liaozhaozhou <liaozhaozhou@ecyao.com>
2023-11-09 18:53:47 +0800

    拉伸指令

--- src/views/example/index.vue
+++ src/views/example/index.vue
@@ -1,79 +1,24 @@
 <template>
-  <main>
-    <div class="example-box">
-      <div class="img-container">
-        <div class="img-item"></div>
-        <div class="img-item"></div>
-        <div class="img-item"></div>
-        <div class="img-item"></div>
-        <div class="img-item"></div>
-        <div class="img-item"></div>
-        <div class="img-item"></div>
-        <div class="img-item"></div>
-        <div class="img-item"></div>
-      </div>
-    </div>
-  </main>
+  <div class="container">
+    <div v-resize.right class="left" />
+    <div class="right" />
+  </div>
 </template>
-
-<script setup lang="ts">
-// import { CssExampla } from './config/common'
-</script>
-
-<style scoped lang="scss">
-main {
-  height: 100%;
-  width: 100%;
-  padding: 5px;
-  border-radius: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.img-container {
-  width: 300px;
-  height: 300px;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  .img-item {
-    box-shadow: inset 0 0 0 1px #fff;
-    transition: 0.5s;
-    background-size: 300px 300px;
-    background-image: url('../../assets/白天.png');
-    position: relative;
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+.container {
+  width: 400px;
+  height: 100px;
+  > div {
+    float: left;
+    height: 100%;
+    width: 50%;
   }
-  // 横向
-  .img-item:nth-child(3n + 1) {
-    background-position-x: 0;
-    left: -20px;
+  .left {
+    background-color: lightcoral;
   }
-  .img-item:nth-child(3n + 2) {
-    background-position-x: -100%;
-    left: 0;
-  }
-  .img-item:nth-child(3n + 3) {
-    background-position-x: -200%;
-    left: 20px;
-  }
-  // 纵向
-  .img-item:nth-child(n + 7) {
-    background-position-y: -200%;
-    top: 20px;
-  }
-  .img-item:nth-child(-n + 6) {
-    background-position-y: -100%;
-    top: 0;
-  }
-  .img-item:nth-child(-n + 3) {
-    background-position-y: 0;
-    top: -20px;
-  }
-  &:hover {
-    .img-item {
-      left: 0;
-      top: 0;
-      box-shadow: inset 0 0 0 0 #fff;
-    }
+  .right {
+    background-color: lightblue;
   }
 }
 </style>
